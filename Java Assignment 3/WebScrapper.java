933a52bd213db3a699d6cbe1047df9bea5c2f8a6
nikhilshaji17 <62615489+nikhilshaji17@users.noreply.github.com>
2020-09-23 21:23:25 +0530

    Add files via upload

---
+++ Java Assignment 3/WebScrapper.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import java.net.URL;
+
+import com.opencsv.CSVWriter;
+import org.jsoup.UnsupportedMimeTypeException;
+import java.net.*;
+
